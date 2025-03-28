bf9c592d04e914356ab195ceac3341bd0b1ead2e
Steam <landon.overton.loa@gmail.com>
2025-03-27 20:04:24 -0500

    Update basic.js

--- examples/basic.js
+++ examples/basic.js
@@ -5,10 +5,16 @@ const client = new ERLCClient('YOUR_API_KEY');
 async function main() {
   try {
     const serverInfo = await client.server.getInfo();
+    const players = await client.server.getPlayers();
+    const bans = await client.server.getBans();
+    const modCalls = await client.server.getModCalls();
+
     console.log('Server Status:', serverInfo.status);
-    
-    const bans = await client.players.getBans();
-    console.log('Active Bans:', bans);
+    console.log('Online Players:', players.length);
+    console.log('Active Bans:', Object.keys(bans).length);
+    console.log('Recent Mod Calls:', modCalls.length);
+
+    await client.commands.send(':h Server restart in 5 minutes!');
   } catch (error) {
     console.error('Error:', error.message);
   }
