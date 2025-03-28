31dd580196cbadc9c480e1d101f0f71cf2fe49b6
Steam <landon.overton.loa@gmail.com>
2025-03-27 20:02:42 -0500

    Update Server.js

--- src/endpoints/Server.js
+++ src/endpoints/Server.js
@@ -7,8 +7,32 @@ class Server {
     return (await this.request.execute('/server')).data;
   }
 
+  async getPlayers() {
+    return (await this.request.execute('/server/players')).data;
+  }
+
   async getQueue() {
     return (await this.request.execute('/server/queue')).data;
+  }
+
+  async getJoinLogs() {
+    return (await this.request.execute('/server/joinlogs')).data;
+  }
+
+  async getKillLogs() {
+    return (await this.request.execute('/server/killlogs')).data;
+  }
+
+  async getCommandLogs() {
+    return (await this.request.execute('/server/commandlogs')).data;
+  }
+
+  async getModCalls() {
+    return (await this.request.execute('/server/modcalls')).data;
+  }
+
+  async getBans() {
+    return (await this.request.execute('/server/bans')).data;
   }
 
   async getVehicles() {
