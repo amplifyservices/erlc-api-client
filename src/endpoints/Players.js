f7e870344b78a0ff05905937406bb167c720b6e1
Steam <landon.overton.loa@gmail.com>
2025-03-27 19:56:37 -0500

    Update Players.js

--- src/endpoints/Players.js
+++ src/endpoints/Players.js
@@ -1,26 +1,19 @@
-const BaseEndpoint = require('./BaseEndpoint');
+class Players {
+  constructor(requestHandler) {
+    this.request = requestHandler;
+  }
 
-class PlayersEndpoint extends BaseEndpoint {
   async getList() {
-    return this.client.execute('/server/players');
-  }
-
-  async getJoinLogs() {
-    return this.client.execute('/server/joinlogs');
-  }
-
-  async getKillLogs() {
-    return this.client.execute('/server/killlogs');
+    return (await this.request.execute('/server/players')).data;
   }
 
   async getBans() {
-    return this.client.execute('/server/bans');
+    return (await this.request.execute('/server/bans')).data;
   }
 
-  async searchPlayer(name) {
-    const players = await this.getList();
-    return players.find(p => p.name.toLowerCase().includes(name.toLowerCase()));
+  async getJoinLogs() {
+    return (await this.request.execute('/server/joinlogs')).data;
   }
 }
 
-module.exports = PlayersEndpoint;
+module.exports = Players;
