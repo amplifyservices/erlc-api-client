1afbff1aa9a901a91182dba91717c0785fff19ed
Steam <landon.overton.loa@gmail.com>
2025-03-27 20:03:25 -0500

    Update Client.js

--- src/core/Client.js
+++ src/core/Client.js
@@ -1,7 +1,6 @@
 const { RateLimiter } = require('./RateLimiter');
 const { RequestHandler } = require('./RequestHandler');
 const Server = require('../endpoints/Server');
-const Players = require('../endpoints/Players');
 const Commands = require('../endpoints/Commands');
 
 class ERLCClient {
@@ -13,7 +12,6 @@ class ERLCClient {
     this.requestHandler = new RequestHandler(this.rateLimiter, options);
     
     this.server = new Server(this.requestHandler);
-    this.players = new Players(this.requestHandler);
     this.commands = new Commands(this.requestHandler);
   }
 }
